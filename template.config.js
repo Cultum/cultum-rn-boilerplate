37b17fc4e5124f5b6e8dc95e2b9a75723ccb3f0d
Eugene Nagornyi <eugen2991@gmail.com>
2022-02-07 18:05:18 +0200

    fix

--- template.config.js
+++ template.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  // Placeholder used to rename and replace in files
-  // package.json, index.json, android/, ios/
-  placeholderName: "CultumRNStarterKit",
+    // Placeholder used to rename and replace in files
+    // package.json, index.json, android/, ios/
+    placeholderName: 'CultumRNStarterKit',
 
-  // Directory with template
-  templateDir: "./template",
+    // Directory with template
+    templateDir: './template',
 
-  // Path to script, which will be executed after init
-  postInitScript: "./script.js"
+    // Path to script, which will be executed after init
+    postInitScript: './post-init.script.sh',
 };
